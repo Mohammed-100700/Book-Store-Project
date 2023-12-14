2de58ce616860020b418de9a8c6c6d9fa20847e4
Mohammed <salimmohammad7890@gmail.com>
2023-12-15 04:12:55 +0600

    Done with the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { CreateBooks, DeleteBook, EditBook, Home, ShowBook } from "./pages";
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
